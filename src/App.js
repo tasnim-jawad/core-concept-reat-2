65dc6263355164cf5a821f973f3340f887a550df
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-05-23 16:24:37 +0600

    useeffct prectice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Counter></Counter>
+        <Usersall></Usersall>
       </header>
     </div>
   );
 }
+
+
 function Counter(){
   const [counter,setCounter] = useState(10);
+
   const handaleIncrease =() =>{
-    setCounter( number =>number + 1);
+    setCounter( previousNumber =>previousNumber + 1);
   };
+
   const handaleDicrease =() =>{
-    setCounter( number =>number - 1);
+    setCounter( previousNumber =>previousNumber - 1);
   };
   return <div>
       <h1>now i have: {counter} /-</h1>
       <button onClick ={handaleIncrease}>increase</button> <button onClick ={handaleDicrease}>dicrease</button>
   </div> 
 }
+
+function Usersall(){
+  const [user,setUser] =useState([]);
+  
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUser(data))
+  },[])
+  console.log(user);
+  return <div>
+            <h1>user list printing: {user.length}</h1>
+            {
+              user.map(allInfo =><p>{allInfo.name}</p>)
+            }
+          </div>
+}
+
+
 
 export default App;
