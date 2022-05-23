e25bccc2e48b8e80c79b5d10061ae54c5e819981
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2022-05-23 11:23:17 +0600

    use-state first time i used

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter></Counter>
       </header>
     </div>
   );
+}
+function Counter(){
+  const [counter,setCounter] = useState(10);
+  const handaleIncrease =() =>{
+    setCounter( number =>number + 1);
+  };
+  const handaleDicrease =() =>{
+    setCounter( number =>number - 1);
+  };
+  return <div>
+      <h1>now i have: {counter} /-</h1>
+      <button onClick ={handaleIncrease}>increase</button> <button onClick ={handaleDicrease}>dicrease</button>
+  </div> 
 }
 
 export default App;
